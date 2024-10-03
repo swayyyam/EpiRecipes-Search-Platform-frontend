0fc1a2540ade5becf7fad009b97899fae7157d02
Swayam Sethi <swayam@gmail.com>
2024-10-03 17:52:26 +0530

    modifications 2

--- src/components/search.js
+++ src/components/search.js
@@ -10,6 +10,12 @@ const Search = () => {
 
     const handleSearch = async (e) => {
         e.preventDefault();
+
+        if (!query) {
+            alert("Search Query not provided!");
+            return;
+        }
+
         setLoading(true); 
         try {
             const response = await axios.get(`http://127.0.0.1:8000/search/?query=${query}&fields=${fields}`);
@@ -68,3 +74,4 @@ const Search = () => {
 };
 
 export default Search;
+
